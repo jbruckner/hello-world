4997c659004450dc642880bd409f183196ec6d8b
Johann Bruckner <bruckner.j@gmx.de>
2018-06-24 11:07:45 +0200

    session ready

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ app.get('/', (req, res) => {
     
     let rawdata = fs.readFileSync('./src/sessions.json'); 
     let data = JSON.parse(rawdata);
+
+    var i;
+    for (i = 0; i < data.sessions.length; i++) {
+        var date = data.sessions[i].date;
+        var timeDif = (new Date()).getTime() - date;
+        if ((30 * 1000) < timeDif) {
+            data.sessions.splice(i, 1);
+        } 
+    }
+
+
     data.sessions.push(newObject);
     fs.writeFileSync('./src/sessions.json', JSON.stringify(data));
 
